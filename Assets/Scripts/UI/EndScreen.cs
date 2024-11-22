79cd4d83652f00451baa5aea7eef6e77e0389296
Jonathan <510031@student.fontys.nl>
2024-11-22 13:01:38 +0100

    Feature - EndScreen Quit

---
+++ Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
